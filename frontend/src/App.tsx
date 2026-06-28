464d44344ac47bad027a673a8efb3527ea14731a
Akshat Agrawal <work.brayill@gmail.com>
2026-06-29 02:01:53 +0530

    Setup of React+Tailwind CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-function App(){
-    return (
-      <div>
-        <h1>CredWise AI</h1>
-        <p>AI Powered Credit Risk Assesment Platform</p>
-      </div>
-    );
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-6xl font-bold text-green-400">
+        CredWise AI
+      </h1>
+    </div>
+  );
 }
+
 export default App;
